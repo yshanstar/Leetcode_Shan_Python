1da8492c4893945d307fa4502f3f16d3cac61faa
shye <shye@microsoft.com>
2014-08-11 17:32:01 -0700

    Remove Element

---
+++ RemoveElement.py
@@ -0,0 +1,16 @@
+__author__ = 'shye'
+class RemoveElement:
+    # @param A a list of integers
+    # @param elem an integer, value need to be removed
+    # @return an integer
+    def removeElement(self, A, elem):
+        length = len(A)
+        if length <= 0:
+            return length
+
+        end = 0
+        for i in A:
+            if i != elem:
+                A[end] = i
+                end += 1
+        return end
